eb0315acdaf34fb62e1e5473ec33aa4821d4e35f
Hao Guan <10684225+hguandl@users.noreply.github.com>
2023-01-10 11:48:41 +1000

    Fix GCC 11 build errors

--- include/adb-lite/expected.hpp
+++ include/adb-lite/expected.hpp
@@ -4,22 +4,8 @@
 #include <version>
 
 #ifdef __cpp_lib_expected
+
 #include <expected>
-#else
-#include <tl/expected.hpp>
-
-namespace std {
-
-template <class E> using unexpected = tl::unexpected<E>;
-
-template <class E> using bad_expected_access = tl::bad_expected_access<E>;
-
-using unexpect_t = tl::unexpect_t;
-
-template <class T, class E> using expected = tl::expected<T, E>;
-
-} // namespace std
-#endif // __cpp_lib_expected
 
 namespace adb {
 
@@ -27,3 +13,16 @@ template <class T = void> using expected = std::expected<T, std::error_code>;
 using unexpected = std::unexpected<std::error_code>;
 
 } // namespace adb
+
+#else // __cpp_lib_expected
+
+#include <tl/expected.hpp>
+
+namespace adb {
+
+template <class T = void> using expected = tl::expected<T, std::error_code>;
+using unexpected = tl::unexpected<std::error_code>;
+
+} // namespace adb
+
+#endif // __cpp_lib_expected
